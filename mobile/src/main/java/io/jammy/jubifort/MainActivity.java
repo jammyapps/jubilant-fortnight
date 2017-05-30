e2eb297a23bdc4ca2b2b28f4ddbcc127337c2248
Matt Fenlon <nolnefm@gmail.com>
2017-05-30 07:44:53 -0700

    Initial Android project setup

---
+++ mobile/src/main/java/io/jammy/jubifort/MainActivity.java
@@ -0,0 +1,134 @@
+package io.jammy.jubifort;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * JubiFort is a boilerplate project for Android. The intention is to trim back the run-up time for new projects by
+ * developing and maintaining a fully-tested project base that contains working, tested examples for performing various
+ * common and/or tricky functions that are required in modern Android apps in order to reduce the frequency of gotchas
+ * and unexpected holdups caused by library incompatibilities and the like.
+ *
+ * The ultimate goal of this project is to provide Android developers with a kit of pre-prepared examples that they can
+ * extend, copy-paste and hack to get an MVP working whilst spending less time on hunting obscure bugs that thousands
+ * of people have seen before. Such that if you need it, copy it. If you don't, rip it out.
+ *
+ * The project includes basic Android Wear and Android Auto support.
+ *
+ * Caveat: This project is designed to serve our purposes. If you need something different, feel free to fork, but at
+ * this point we intend to keep the focus sharp: "provide one reliable solution to each identified problem"
+ *
+ * TODO - Database/Non-SharePrefs data storage and retrieval system
+ * TODO - Android Auto notifications tie-in
+ * TODO - Android Wear notification tie-in
+ * TODO - JavaDocs
+ * TODO - RAMLgen
+ * TODO - UI testing (multi-application - UI Automator)
+ * TODO - UI testing (Espresso)
+ * TODO - Testing and mocking
+ * TODO - Dev/Stg/Prd flavours
+ * TODO - Internationalisation examples
+ * TODO - Camera
+ * TODO - Map activity
+ * TODO - NavBar
+ * TODO - BottomSheet
+ * TODO - CheckStyle
+ * TODO - DataBinding examples
+ *
+ */
+public class MainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show();
+      }
+    });
+
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    drawer.setDrawerListener(toggle);
+    toggle.syncState();
+
+    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
+  }
+
+  @Override
+  public void onBackPressed() {
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    if (drawer.isDrawerOpen(GravityCompat.START)) {
+      drawer.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  @SuppressWarnings("StatementWithEmptyBody")
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+
+    if (id == R.id.nav_camera) {
+      // Handle the camera action
+    } else if (id == R.id.nav_gallery) {
+
+    } else if (id == R.id.nav_slideshow) {
+
+    } else if (id == R.id.nav_manage) {
+
+    } else if (id == R.id.nav_share) {
+
+    } else if (id == R.id.nav_send) {
+
+    }
+
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    drawer.closeDrawer(GravityCompat.START);
+    return true;
+  }
+}
---
+++ wear/src/main/java/io/jammy/jubifort/MyStubBroadcastActivity.java
@@ -0,0 +1,22 @@
+package io.jammy.jubifort;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    Intent i = new Intent();
+    i.setAction("io.jammy.jubifort.SHOW_NOTIFICATION");
+    i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+    sendBroadcast(i);
+    finish();
+  }
+}
